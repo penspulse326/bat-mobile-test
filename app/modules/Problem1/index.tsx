347c5a54eb3197f8f9e416cc56df74dce67a3c0c
SHIN <akiyachiumi@gmail.com>
2024-04-22 13:54:47 +0800

    feat: add calculate logic

---
+++ app/modules/Problem1/index.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import calculateSum from '@/app/common/helper/calculateSum';
+import { useRef, useState } from 'react';
+
+function Problem1() {
+  const [value, setValue] = useState(0);
+  const [sum, setSum] = useState(0);
+  const [spentTime, setSpentTime] = useState(0);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const lastValueRef = useRef(0);
+
+  const handleCalulate = () => {
+    // 例外排除
+    if (!value || value <= 0) {
+      setErrorMessage('請輸入正整數');
+      return;
+    }
+
+    // 初始化
+    setErrorMessage('');
+    lastValueRef.current = value;
+
+    // 計算
+    const { result, totalSpentTime } = calculateSum(value);
+
+    setSum(result);
+    setSpentTime(totalSpentTime);
+    setValue(0);
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      請輸入 N 後按下 Enter 或輸入按鈕進行計算
+      <div className="flex">
+        <input
+          type="number"
+          className="p-2"
+          value={value || ''}
+          onChange={(e) => setValue(parseInt(e.target.value, 10))}
+          onKeyDown={(e) => e.key === 'Enter' && handleCalulate()}
+        />
+        <button
+          type="button"
+          className="ml-2 bg-white px-2 py-1"
+          onClick={handleCalulate}
+        >
+          計算
+        </button>
+      </div>
+      {errorMessage && <div className="text-red-500">{errorMessage}</div>}
+      <div>
+        N 為 {lastValueRef.current}，總合為：{sum}
+      </div>
+      <div>共花費：{spentTime} 毫秒</div>
+      <hr className="w-full border-gray-500" />
+      <p className="text-center text-sm">
+        計算說明：
+        <br /> N 為奇數時的規律：總合為： N/2 + 1 <br />N
+        為偶數時的規律，總合為： N/2 + 2
+      </p>
+    </div>
+  );
+}
+
+export default Problem1;
--- app/problem1/page.tsx
+++ app/problem1/page.tsx
@@ -1,16 +1,12 @@
+import Problem1 from '../modules/Problem1';
+
 function Problem1Page() {
   return (
     <main className="flex min-h-[100dvh] flex-col items-center gap-4 p-24">
       <h1 className="text-2xl">第 1 題</h1>
       <p>寫一個函式計算下列公式之總和： 1+2-3+4-5+6-.....+ 或 - N</p>
-      <div className="h-[1px] w-full bg-gray-500" />
-      請輸入 N 後按下 Enter 或輸入按鈕進行計算
-      <div className="flex">
-        <input type="text" className="p-2" />
-        <button type="button" className="ml-2 bg-white px-2 py-1">
-          計算
-        </button>
-      </div>
+      <hr className="w-full border-gray-500" />
+      <Problem1 />
     </main>
   );
 }
