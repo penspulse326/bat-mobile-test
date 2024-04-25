b987fdff33a204a29971e2f2ff4e459620a21fed
SHIN <akiyachiumi@gmail.com>
2024-04-26 01:05:01 +0800

    feat: add CitySelect button state

--- components/Selects/CitySelect/index.tsx
+++ components/Selects/CitySelect/index.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 
 function CitySelect() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <button
       type="button"
+      onClick={() => setIsOpen(!isOpen)}
       className="relative flex h-10 w-[175px] items-center justify-between rounded-lg bg-grey-light px-4 py-[10px]"
     >
       {/* 注意文字粗體 */}
@@ -15,12 +21,14 @@ function CitySelect() {
         width={18}
         height={18}
       />
-      <ul className="absolute left-0 top-[100%] mt-3 flex w-full flex-col gap-4 rounded-lg bg-grey-light p-4 text-left">
-        <li>台北市</li>
-        <li>台北市</li>
-        <li>台北市</li>
-        <li>台北市</li>
-      </ul>
+      {isOpen && (
+        <ul className="absolute left-0 top-[100%] mt-3 flex w-full flex-col gap-4 rounded-lg bg-grey-light p-4 text-left">
+          <li>台北市</li>
+          <li>台北市</li>
+          <li>台北市</li>
+          <li>台北市</li>
+        </ul>
+      )}
     </button>
   );
 }
