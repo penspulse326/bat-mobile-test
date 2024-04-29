e81a7f2117b89785b80fe55909b49c9ec2f000e3
SHIN <akiyachiumi@gmail.com>
2024-04-29 11:47:35 +0800

    fixed: fix Select's style

--- components/Select/index.tsx
+++ components/Select/index.tsx
@@ -17,15 +17,18 @@ function Select({ value, onChange }: PropsType) {
       onClick={() => setIsOpen(!isOpen)}
       className="relative flex h-10 w-[311px] items-center justify-between rounded-lg bg-grey-light px-4 py-[10px] text-lg sm:w-[50%] lg:w-[175px]"
     >
-      {/* 注意文字粗體 */}
       <span className={`${value ? 'text-grey-dark' : 'text-grey'} font-medium`}>
         {value || '選擇縣市'}
       </span>
       <Icon isSelected={value === ''} />
       {isOpen && (
-        <ul className="absolute left-0 top-[100%] z-10 mt-3 flex max-h-[500px] w-full flex-col gap-4 overflow-y-scroll rounded-lg bg-grey-light p-4 text-left">
+        <ul className="absolute left-0 top-[100%] z-10 mt-3 flex max-h-[500px] w-full flex-col overflow-y-scroll rounded-lg bg-grey-light py-2 text-left">
           {cities.map((city: string) => (
-            <li key={city} onClick={() => onChange(city)}>
+            <li
+              key={city}
+              onClick={() => onChange(city)}
+              className="px-4 py-2 hover:bg-grey-dark hover:text-white"
+            >
               {city}
             </li>
           ))}
